c17e9ddec330d099081e6a16dbff600406643bd8
Andi Rachmadian <andikharisma24@gmail.com>
2020-04-27 20:22:48 +0700

    edit header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,62 +4,43 @@ import { NavLink } from 'react-router-dom';
 export default function Header() {
   return (
     <>
-      <div className='container is-fullhd'>
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
-          <div className='navbar-brand'>
-            <a className='navbar-item' href='#'>
-              <img src={`${process.env.PUBLIC_URL}/image/logoCilsy/Cilsyplus..png`} />
-            </a>
-
-            <a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
-            </a>
-          </div>
-
-          <div id='navbarBasicExample' className='navbar-menu'>
-            <div className='navbar-start'>
-              <NavLink to='/' className='navbar-item'>
-                Home
-              </NavLink>
-
-              <NavLink to='/books/2' className='navbar-item'>
-                BookDetail
-              </NavLink>
-
-              <div className='navbar-item has-dropdown is-hoverable'>
-                <a className='navbar-link'>More</a>
-
-                <div className='navbar-dropdown'>{/* <a className="navbar-item">
-            About
+      <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand is-4'>
+          <a className='navbar-item' href='#'>
+            <img src={`${process.env.PUBLIC_URL}/image/logoCilsy/Cilsyplus..png`} width='112' height='28' />
           </a>
-          <a className="navbar-item">
-            Jobs
-          </a>
-          <a className="navbar-item">
-            Contact
-          </a>
-          <hr className="navbar-divider">
-          <a className="navbar-item">
-            Report an issue
-          </a> */}</div>
-              </div>
-            </div>
+        </div>
 
-            <div className='navbar-end'>
-              <div className='navbar-item'>
-                <div className='buttons'>
-                  <a className='button is-primary'>
-                    <strong>Sign up</strong>
-                  </a>
-                  <a className='button is-light'>Log in</a>
+        <div id='navbarBasicExample' className='navbar-menu'>
+          <div className='navbar-start'>
+            <div className='navbar-item has-dropdown is-hoverable'>
+              <a className='navbar-link'>Categories</a>
+
+              <div className='navbar-dropdown'>
+                <div className='navbar-item'>
+                  <div className='columns is-multiline is-full'>
+                    <div className='column is-one-third'>abc</div>
+                    <div className='column is-one-third'>abc</div>
+                    <div className='column is-one-third'>abc</div>
+                  </div>
                 </div>
               </div>
+              <input className='input' type='text' placeholder='Search book  title or author'></input>
             </div>
           </div>
-        </nav>
-      </div>
+
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <div className='buttons'>
+                <a className='button is-primary'>
+                  <strong>Sign up</strong>
+                </a>
+                <a className='button is-light'>Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
