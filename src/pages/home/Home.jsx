02495061926f37e31ea47d8f7b157641c62d2690
Andi Rachmadian <andikharisma24@gmail.com>
2020-04-28 23:53:39 +0700

    raw content home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>isi</div>
+      <div className='container'>
+        <div>CAROUSEL</div>
+        <div>
+          <h2>Popular Categories</h2>
+          <br />
+          <div className='columns'>
+            <div className='box column'>
+              <h3>Self Development</h3>
+            </div>
+            <div className='box column'>
+              <h3>Engineering</h3>
+            </div>
+            <div className='box column'>
+              <h3>Novel</h3>
+            </div>
+            <div className='box column'>
+              <h3>Computer Science</h3>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 };
