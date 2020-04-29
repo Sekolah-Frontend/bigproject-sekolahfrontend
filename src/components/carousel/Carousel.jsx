05b730eca31bbfbe01b7e83f82df7aad12a554b4
Andi Rachmadian <andikharisma24@gmail.com>
2020-04-29 21:45:53 +0700

    carousel edit

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,10 +5,10 @@ import '@brainhubeu/react-carousel/lib/style.css';
 const CarouselCilsy = () => {
   return (
     <>
-      <Carousel centered infinite arrows dots slidesPerPage={2}>
-        <img src='https://ecs7.tokopedia.net/img/banner/2020/4/27/85531617/85531617_2662a536-c03e-48fc-b8b0-7af2d075995b.jpg' />
-        <img src={`${process.env.PUBLIC_URL}/images/carousel/Artboard-5@3x.png`} />
-        <img src={`${process.env.PUBLIC_URL}/images/carousel/Artboard-5@3x.png`} />
+      <Carousel centered infinite arrows dots slidesPerPage={1} offset={1}>
+        <img src='/image/carousel/Artboard4@3x.png' />
+        <img src='/image/carousel/Artboard5@3x.png' />
+        <img src='/image/carousel/Artboard5@3x.png' />
       </Carousel>
     </>
   );
